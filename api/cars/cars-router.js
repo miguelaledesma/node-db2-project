f9c9b28d5b17f1ffa814b4277ecee8e73eb1f4b1
Miguel A. Ledesma <miguelledesma33@gmail.com>
2022-04-19 14:05:43 -0700

    project is wired up now just need to create the model, middleware and route enpoints

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,22 @@
 // DO YOUR MAGIC
+
+const express = require('express'); 
+
+const router = express.Router(); 
+
+
+
+router.get('/' , async (req, res, next) => {
+    res.json('hello')
+})
+
+router.get('/:id' , async (req, res, next) => {
+    res.json('hello im getting ')
+})
+
+router.post('/' , async (req, res, next) => {
+    res.json('hello im posting')
+})
+
+
+module.exports = router 
